061e918129a5858da5e15eef040fd5c68d2ac781
Talha38ua <48105452+Talha38ua@users.noreply.github.com>
2020-11-13 01:10:55 +0100

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -33,7 +33,7 @@ void loop() {
   if (digitalRead(switch_pin) == HIGH) {
     servoPosition = 10;
     myservo.write(servoPosition);              // tell servo to go to position in variable "servoPosition"
-    if (sensorValue < 800) {
+    if (sensorValue < 100) {
       gameOfThrones();
     }
   }
